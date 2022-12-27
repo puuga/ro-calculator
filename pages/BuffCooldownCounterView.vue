af03dd7d869edcf615325bb9c9a1d2a28308fbbb
Siwawes Wongcharoen <siwaoh@gmail.com>
2022-12-27 23:17:42 +0700

    feat: improve code - fix content - refactor

--- pages/BuffCooldownCounterView.vue
+++ pages/BuffCooldownCounterView.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import consola from 'consola'
+import { getAnalytics, logEvent } from 'firebase/analytics'
 
+import { RN_BUFF_COOLDOWN_COUNTER_TOOL, findRouteByName } from '@/app/router.options'
+import { e } from 'unimport/dist/types-488f6d65';
+
+//#region use...
+const route = findRouteByName(RN_BUFF_COOLDOWN_COUNTER_TOOL)
+const _title = route?.title || 'Cooldown Counter | RO-Calculator'
+const _description = route?.description || 'Cooldown Counter Tool'
 useHead({
-  title: 'Cooldown Counter | RO-Calculator',
+  title: _title,
   meta: [
-    { name: 'description', content: 'Cooldown Counter' }
+    { name: 'description', content: _description }
   ],
 })
 
+const { $firebaseApp } = useNuxtApp()
+//#endregion use...
+
+//#region refs
 const sound = ref('')
 const soundOptions = [
   {
@@ -31,38 +43,38 @@ const buffs = ref([])
 const buffsOptions = [
   {
     id: 'buff_30s',
-    title: 'Buff 30s',
+    title: 'Cooldown 30 s (0:30 m)',
     value: 'buff_30s'
   },
   {
     id: 'buff_60s',
-    title: 'Buff 60s (1 m)',
+    title: 'Cooldown 60 s (1:00 m)',
     value: 'buff_60s'
   },
   {
     id: 'buff_90s',
-    title: 'Buff 90s (1:30 m)',
+    title: 'Cooldown 90 s (1:30 m)',
     value: 'buff_90s'
   },
   {
     id: 'buff_120s',
-    title: 'Buff 120s (2 m)',
+    title: 'Cooldown 120 s (2:00 m)',
     value: 'buff_120s'
   },
   {
     id: 'buff_180s',
-    title: 'Buff 180s (3 m)',
+    title: 'Cooldown 180 s (3:00 m)',
     value: 'buff_180s'
   },
   {
     id: 'buff_300s',
-    title: 'Buff 300s (5 m)',
+    title: 'Cooldown 300 s (5:00 m)',
     value: 'buff_300s'
   },
   {
-    id: 'buff_6000s',
-    title: 'Buff 6000s (10 m)',
-    value: 'buff_6000s'
+    id: 'buff_600s',
+    title: 'Cooldown 600 s (10:00 m)',
+    value: 'buff_600s'
   },
 ]
 
@@ -72,7 +84,7 @@ const clockCooldown90s = ref(false)
 const clockCooldown120s = ref(false)
 const clockCooldown180s = ref(false)
 const clockCooldown300s = ref(false)
-const clockCooldown6000s = ref(false)
+const clockCooldown600s = ref(false)
 
 const clockCooldown30sInterval = ref('')
 const clockCooldown60sInterval = ref('')
@@ -80,7 +92,7 @@ const clockCooldown90sInterval = ref('')
 const clockCooldown120sInterval = ref('')
 const clockCooldown180sInterval = ref('')
 const clockCooldown300sInterval = ref('')
-const clockCooldown6000sInterval = ref('')
+const clockCooldown600sInterval = ref('')
 
 let clockCooldown30sCountdownInterval: any
 let clockCooldown60sCountdownInterval: any
@@ -88,9 +100,10 @@ let clockCooldown90sCountdownInterval: any
 let clockCooldown120sCountdownInterval: any
 let clockCooldown180sCountdownInterval: any
 let clockCooldown300sCountdownInterval: any
-let clockCooldown6000sCountdownInterval: any
+let clockCooldown600sCountdownInterval: any
+//#endregion refs
 
-
+//#region methods
 function onFormSubmit(event: Event) {
   event.preventDefault()
 
@@ -98,41 +111,48 @@ function onFormSubmit(event: Event) {
   consola.log('BuffCooldownCounterView onFormSubmit sound', sound.value)
   consola.log('BuffCooldownCounterView onFormSubmit buffs', buffs.value)
 
-  if ((buffs.value as string[]).includes('buff_30s')) {
+  const _buffs = buffs.value as string[]
+  if (_buffs.includes('buff_30s')) {
     clockCooldown30s.value = true
     consola.log('BuffCooldownCounterView clockCooldown30s')
     clockCooldown30sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_60s')) {
+  if (_buffs.includes('buff_60s')) {
     clockCooldown60s.value = true
     consola.log('BuffCooldownCounterView clockCooldown60s')
     clockCooldown60sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_90s')) {
+  if (_buffs.includes('buff_90s')) {
     clockCooldown90s.value = true
     consola.log('BuffCooldownCounterView clockCooldown90s')
     clockCooldown90sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_120s')) {
+  if (_buffs.includes('buff_120s')) {
     clockCooldown120s.value = true
     consola.log('BuffCooldownCounterView clockCooldown120s')
     clockCooldown120sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_180s')) {
+  if (_buffs.includes('buff_180s')) {
     clockCooldown180s.value = true
     consola.log('BuffCooldownCounterView clockCooldown180s')
     clockCooldown180sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_300s')) {
+  if (_buffs.includes('buff_300s')) {
     clockCooldown300s.value = true
     consola.log('BuffCooldownCounterView clockCooldown300s')
     clockCooldown300sCountdown(sound.value)
   }
-  if ((buffs.value as string[]).includes('buff_6000s')) {
-    clockCooldown6000s.value = true
-    consola.log('BuffCooldownCounterView clockCooldown6000s')
-    clockCooldown6000sCountdown(sound.value)
+  if (_buffs.includes('buff_600s')) {
+    clockCooldown600s.value = true
+    consola.log('BuffCooldownCounterView clockCooldown600s')
+    clockCooldown600sCountdown(sound.value)
   }
+
+  const analytics = getAnalytics($firebaseApp())
+  logEvent(analytics, "CooldownCounter", {
+    buffs: _buffs,
+    sound: sound.value
+  });
 }
 
 function previewSound(soundPath: string) {
@@ -167,9 +187,9 @@ function stopCountdown() {
   clockCooldown300s.value = false
   clockCooldown300sInterval.value = ''
 
-  clearInterval(clockCooldown6000sCountdownInterval)
-  clockCooldown6000s.value = false
-  clockCooldown6000sInterval.value = ''
+  clearInterval(clockCooldown600sCountdownInterval)
+  clockCooldown600s.value = false
+  clockCooldown600sInterval.value = ''
 }
 
 function clockCooldown30sCountdown(soundPath: string) {
@@ -274,42 +294,57 @@ function clockCooldown300sCountdown(soundPath: string) {
     }
   }, 1000)
 }
-function clockCooldown6000sCountdown(soundPath: string) {
-  clockCooldown6000s.value = true
-  let seconds = 6000
-  clockCooldown6000sCountdownInterval = setInterval(() => {
+function clockCooldown600sCountdown(soundPath: string) {
+  clockCooldown600s.value = true
+  let seconds = 600
+  clockCooldown600sCountdownInterval = setInterval(() => {
     seconds--
-    consola.log('BuffCooldownCounterView clockCooldown6000sCountdown', seconds)
-    clockCooldown6000sInterval.value = seconds.toString()
+    consola.log('BuffCooldownCounterView clockCooldown600sCountdown', seconds)
+    clockCooldown600sInterval.value = seconds.toString()
     if (seconds <= 0) {
-      clearInterval(clockCooldown6000sCountdownInterval)
-      clockCooldown6000s.value = false
+      clearInterval(clockCooldown600sCountdownInterval)
+      clockCooldown600s.value = false
       const audio = new Audio(soundPath)
       audio.play()
 
-      clockCooldown6000sCountdown(soundPath)
+      clockCooldown600sCountdown(soundPath)
     }
   }, 1000)
 }
+//#endregion methods
 </script>
 
 <template>
   <main class="container mx-auto">
-    <h1 class="text-4xl font-bold">BuffCooldownCounterView</h1>
+    <h1 class="text-4xl font-bold">{{ _title }}</h1>
 
     <div>SEO: Cooldown Counter</div>
 
     <div class="flex">
       <!-- #region form -->
-      <div>
+      <div class="w-6/12 c-card m-1">
+        <div class="text-2xl">Config</div>
         <form @submit="onFormSubmit">
           <!-- #region input sound -->
           <div>
             <div class="text-xl">Sound</div>
             <div v-for="option in soundOptions" :key="option.id" class="my-1">
-              <button type="button" @click="previewSound(option.value)" class="c-btn-primary">Preview Sound</button>
-              <input type="radio" v-model="sound" :id="option.id" name="soundInput" :value="soundOptions[0].value"
-                class="mx-2" required />
+              <button 
+                type="button" 
+                class="c-btn-primary"
+                @click="previewSound(option.value)" 
+              >
+                ▶️
+              </button>
+              <input 
+                type="radio" 
+                v-model="sound" 
+                :id="option.id" 
+                :value="soundOptions[0].value"
+                name="soundInput" 
+                class="mx-2" 
+                required 
+              />
               <label :for="option.id">{{ option.title }}</label>
             </div>
           </div>
@@ -333,9 +368,9 @@ function clockCooldown6000sCountdown(soundPath: string) {
 
           <!-- #region submit -->
           <div>
-            <button type="submit" class="c-btn-primary">Start</button>
-            <button type="button" class="c-btn-primary mx-1" @click="stopCountdown()">Stop</button>
-            <button type="reset" class="c-btn-primary">Reset</button>
+            <button type="submit" class="c-btn-primary w-full my-1">Start</button>
+            <button type="button" class="c-btn-secondary w-full my-1" @click="stopCountdown()">Stop</button>
+            <button type="reset" class="c-btn-secondary w-full my-1">Reset</button>
           </div>
           <!-- #endregion submit -->
         </form>
@@ -343,15 +378,43 @@ function clockCooldown6000sCountdown(soundPath: string) {
       <!-- #endregion form -->
 
       <!-- #region clock cooldown -->
-      <div class="grow">
-        <div class="">Cooldown</div>
-        <div v-if="clockCooldown30s">buff_30s: {{ clockCooldown30sInterval }}</div>
-        <div v-if="clockCooldown60s">buff_60s: {{ clockCooldown60sInterval }}</div>
-        <div v-if="clockCooldown90s">buff_90s: {{ clockCooldown90sInterval }}</div>
-        <div v-if="clockCooldown120s">buff_120s: {{ clockCooldown120sInterval }}</div>
-        <div v-if="clockCooldown180s">buff_180s: {{ clockCooldown180sInterval }}</div>
-        <div v-if="clockCooldown300s">buff_300s: {{ clockCooldown300sInterval }}</div>
-        <div v-if="clockCooldown6000s">buff_6000s: {{ clockCooldown6000sInterval }}</div>
+      <div class="w-6/12 c-card m-1">
+        <div class="text-2xl">Cooldown Counter</div>
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown30s"
+          :badge-text="buffsOptions[0].title" 
+          :counter-text="clockCooldown30sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown60s"
+          :badge-text="buffsOptions[1].title" 
+          :counter-text="clockCooldown60sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown90s"
+          :badge-text="buffsOptions[2].title" 
+          :counter-text="clockCooldown90sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown120s"
+          :badge-text="buffsOptions[3].title" 
+          :counter-text="clockCooldown120sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown180s"
+          :badge-text="buffsOptions[4].title" 
+          :counter-text="clockCooldown180sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown300s"
+          :badge-text="buffsOptions[5].title" 
+          :counter-text="clockCooldown300sInterval"
+        />
+        <LazyBuffCooldownCounterClockTextViewV1 
+          v-if="clockCooldown600s"
+          :badge-text="buffsOptions[6].title" 
+          :counter-text="clockCooldown600sInterval"
+        />
       </div>
       <!-- #region clock cooldown -->
     </div>
