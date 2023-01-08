2b44d85dccbc7e9b7dada1fb9317fc38a0ec69e9
Siwawes Wongcharoen <siwaoh@gmail.com>
2023-01-08 22:45:14 +0700

    build: add google adsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
         // <meta name="description" content="My amazing site">
         { name: 'description', content: 'My amazing site.' }
       ],
+      script: [
+        { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6174292774814788', async: true, crossorigin: 'anonymous' },
+      ],
     },
   },
   css: ['~/assets/css/main.css'],
