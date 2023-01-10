ad466982c6aa87dfbfe056066cb9b402baf2d55d
Siwawes Wongcharoen <siwaoh@gmail.com>
2023-01-10 21:51:43 +0700

    feat: finish

---
+++ components/ConsentViewV1.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="mt-4 py-4 bg-slate-400">
+    <div class="container mx-auto">
+      <h1 class="text-2xl font-bold">Consent</h1>
+      <div>
+        เว็บไซต์นี้ ใช้งานระบบ Google Analytics (GA4) ฉะนั้นจะมีการเก็บข้อมูลของท่านผ่านระบบ Google Analytics (GA4) อย่างแน่นอน หากท่านไม่ต้องการให้เก็บข้อมูลของท่าน ให้หยุดใช้งานเว็บไซต์นี้ทันที
+      </div>
+    </div>
+  </div>
+</template>
