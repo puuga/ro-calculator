a94aa8185c6cba834c488ebc78fcd23d159ffc63
Siwawes Wongcharoen <siwaoh@gmail.com>
2023-01-12 22:59:29 +0700

    build: add domain verify #2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
       title: 'RO-Calculator',
       meta: [
         // <meta name="description" content="My amazing site">
-        { hid: 'description', name: 'description', content: 'My amazing site.' }
+        { hid: 'description', name: 'description', content: 'My amazing site.' },
+        { hid: 'google-site-verification', name: 'google-site-verification', content: 'kXt8f679wMbmWGxorwLli467A1-wSC9nbi0aX3wIQNs' },
       ],
       script: [
         { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6174292774814788', async: true, crossorigin: 'anonymous' },
