fa3ec3f91a7d1b8dd71bfb1ee30ff5bf0921c4be
Siwawes Wongcharoen <siwaoh@gmail.com>
2024-06-21 11:09:27 +0700

    feat: update content

--- components/ro-skills/RKSonicWave.vue
+++ components/ro-skills/RKSonicWave.vue
@@ -5,7 +5,7 @@ import { consola } from 'consola';
 
 
 export default defineComponent({
-  name: "RKEnchantBlade",
+  name: "RKSonicWave",
 
   mounted() {
     consola.log("RKSonicWave mounted");
@@ -20,11 +20,28 @@ export default defineComponent({
         { level: 4, desc: "+900 % ATK" },
         { level: 5, desc: "+1000 % ATK" },
       ],
+      skills20240621: [
+        { level: 1, desc: "+1200 % ATK" },
+        { level: 2, desc: "+1350 % ATK" },
+        { level: 3, desc: "+1500 % ATK" },
+        { level: 4, desc: "+1650 % ATK" },
+        { level: 5, desc: "+1800 % ATK" },
+        { level: 6, desc: "+1950 % ATK" },
+        { level: 7, desc: "+2100 % ATK" },
+        { level: 8, desc: "+2250 % ATK" },
+        { level: 9, desc: "+2400 % ATK" },
+        { level: 10, desc: "+2550 % ATK" },
+      ],
       calculation20220721: {
         baseLevel: 100,
         skillLevel: 1,
         atk: 0,
         hit: 0,
+      },
+      calculation20240621: {
+        baseLevel: 100,
+        skillLevel: 1,
+        atk: 0,
       },
     };
   },
@@ -39,10 +56,23 @@ export default defineComponent({
       this.calculation20220721.hit = hit;
 
       const analytics = getAnalytics(this.$firebaseApp());
-      logEvent(analytics, "RKEnchantBlade_calculation20220721ATK", {
+      logEvent(analytics, "RKSonicWave_calculation20220721ATK", {
         baseLevel: baseLevel,
         skillLevel: skillLevel,
         hit: hit,
+        value: atk,
+      });
+    },
+
+    calculation20240621ATK() {
+      const { baseLevel, skillLevel } = this.calculation20240621;
+      const atk = (skillLevel + 7) * 150 * (1 + (baseLevel - 100) / 100);
+      this.calculation20240621.atk = atk;
+
+      const analytics = getAnalytics(this.$firebaseApp());
+      logEvent(analytics, "RKSonicWave_calculation20240621ATK", {
+        baseLevel: baseLevel,
+        skillLevel: skillLevel,
         value: atk,
       });
     },
@@ -55,25 +85,78 @@ export default defineComponent({
     <h2 class="text-2xl font-bold">RK Sonic Wave</h2>
 
     <div class="p-1">
-      <h3 class="text-xl font-bold">Old</h3>
-      <table class="c-table">
-        <thead>
-          <tr>
-            <th class="c-table-th">Level</th>
-            <th class="c-table-th">Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="skill in skills" :key="skill.level">
-            <td class="c-table-td">{{ skill.level }}</td>
-            <td class="c-table-td">{{ skill.desc }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <h3 class="text-xl font-bold">New</h3>
+
+      <div>
+        <span class="c-badge">Update 2024-06-21</span>
+      </div>
+
+      <div>
+        <label for="base-level-20240621" class="c-input-label">
+          Base Level (100-200):
+        </label>
+        <input
+          type="number"
+          id="base-level-20240621"
+          v-model="calculation20240621.baseLevel"
+          min="100"
+          max="200"
+          step="1"
+          class="c-input-number"
+        />
+      </div>
+
+      <div>
+        <label for="skill-level-20240621" class="c-input-label">
+          Skill Level (1-10):
+        </label>
+        <input
+          type="number"
+          id="skill-level-20240621"
+          v-model="calculation20240621.skillLevel"
+          min="1"
+          max="10"
+          step="1"
+          class="c-input-number"
+        />
+      </div>
+
+      <div>
+        <button class="c-btn-primary" @click="calculation20240621ATK()">
+          Calculate
+        </button>
+      </div>
+
+      <div>
+        <strong>
+          Damage = ATK {((Skill level + 7)x150) x (1+[(BaseLV-100)/100])}%
+        </strong>
+      </div>
+
+      <div>
+        <strong>ATK% = {{ calculation20240621.atk }}</strong>
+      </div>
+
+      <div>
+        <table class="c-table">
+          <thead>
+            <tr>
+              <th class="c-table-th">Level</th>
+              <th class="c-table-th">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="skill in skills20240621" :key="skill.level">
+              <td class="c-table-td">{{ skill.level }}</td>
+              <td class="c-table-td">{{ skill.desc }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 
     <div class="p-1">
-      <h3 class="text-xl font-bold">New</h3>
+      <h3 class="text-xl font-bold">Old</h3>
 
       <div>
         <span class="c-badge">Update 2022-07-21</span>
@@ -126,6 +209,24 @@ export default defineComponent({
         <br />
         <strong>Bonus Hit = {{ calculation20220721.hit }}</strong>
       </div>
+    </div>
+
+    <div class="p-1">
+      <h3 class="text-xl font-bold">Old</h3>
+      <table class="c-table">
+        <thead>
+          <tr>
+            <th class="c-table-th">Level</th>
+            <th class="c-table-th">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="skill in skills" :key="skill.level">
+            <td class="c-table-td">{{ skill.level }}</td>
+            <td class="c-table-td">{{ skill.desc }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
--- pages/Lab5HeadgearEnchantmentView.vue
+++ pages/Lab5HeadgearEnchantmentView.vue
@@ -123,6 +123,141 @@ const table2021Enchantments = [
     },
   },
 ];
+const table2024Enchantments = [
+  // Tenacity
+  {
+    name: "Tenacity",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "Melee DMG +4%, Perfect HIT +1%"],
+        ["2", "Melee DMG +8%, Perfect HIT +2%"],
+        ["3", "Melee DMG +12%, Perfect HIT +3%"],
+        ["4", "Melee DMG +16%, Perfect HIT +4%"],
+        ["5", "Melee DMG +20%, Perfect HIT +5%"],
+        ["6", "Melee DMG +24%, Perfect HIT +6%"],
+        ["7", "Melee DMG +28%, Perfect HIT +7%"],
+        ["8", "Melee DMG +32%, Perfect HIT +8%"],
+        ["9", "Melee DMG +36%, Perfect HIT +9%"],
+        ["10", "Melee DMG +44%, Perfect HIT +10%"],
+      ],
+    },
+  },
+  // Mettle
+  {
+    name: "Mettle",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "ATK +4%, HIT +10"],
+        ["2", "ATK +8%, HIT +20"],
+        ["3", "ATK +12%, HIT +30"],
+        ["4", "ATK +16%, HIT +40"],
+        ["5", "ATK +20%, HIT +50"],
+        ["6", "ATK +24%, HIT +60"],
+        ["7", "ATK +28%, HIT +70"],
+        ["8", "ATK +32%, HIT +80"],
+        ["9", "ATK +36%, HIT +90"],
+        ["10", "ATK +44%, HIT +100"],
+      ],
+    },
+  },
+  // Magic Essence
+  {
+    name: "Magic Essence",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "MAtk +4%, Reduces fixed casting time by 0.1 seconds."],
+        ["2", "MAtk +8%, Reduces fixed casting time by 0.2 seconds."],
+        ["3", "MAtk +12%, Reduces fixed casting time by 0.3 seconds."],
+        ["4", "MAtk +16%, Reduces fixed casting time by 0.4 seconds."],
+        ["5", "MAtk +20%, Reduces fixed casting time by 0.5 seconds."],
+        ["6", "MAtk +24%, Reduces fixed casting time by 0.6 seconds."],
+        ["7", "MAtk +28%, Reduces fixed casting time by 0.7 seconds."],
+        ["8", "MAtk +32%, Reduces fixed casting time by 0.8 seconds."],
+        ["9", "MAtk +36%, Reduces fixed casting time by 0.9 seconds."],
+        ["10", "MAtk +44%, Reduces fixed casting time by 1 second."],
+      ],
+    },
+  },
+  // Master Archer
+  {
+    name: "Master Archer",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "ASPD +1%, +physical range damage by 4%"],
+        ["2", "ASPD +2%, +physical range damage by 8%"],
+        ["3", "ASPD +3%, +physical range damage by 12%"],
+        ["4", "ASPD +4%, +physical range damage by 16%"],
+        ["5", "ASPD +5%, +physical range damage by 20%"],
+        ["6", "ASPD +6%, +physical range damage by 24%"],
+        ["7", "ASPD +7%, +physical range damage by 28%"],
+        ["8", "ASPD +8%, +physical range damage by 32%"],
+        ["9", "ASPD +9%, +physical range damage by 36%"],
+        ["10", "ASPD +10%, +physical range damage by 44%"],
+      ],
+    },
+  },
+  // Adamantine
+  {
+    name: "Adamantine",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "Max HP +5%, DEF +140, MDEF +20"],
+        ["2", "Max HP +10%, DEF +180, MDEF +40"],
+        ["3", "Max HP +15%, DEF +220, MDEF +60"],
+        ["4", "Max HP +20%, DEF +260, MDEF +80"],
+        ["5", "Max HP +25%, DEF +300, MDEF +100"],
+        ["6", "Max HP +30%, DEF +340, MDEF +120"],
+        ["7", "Max HP +35%, DEF +380, MDEF +140"],
+        ["8", "Max HP +40%, DEF +420, MDEF +160"],
+        ["9", "Max HP +45%, DEF +460, MDEF +180"],
+        ["10", "Max HP +55%, DEF +500, MDEF +200"],
+      ],
+    },
+  },
+  // Affection
+  {
+    name: "Affection",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "Max SP +3%, +recovery rate of recovery skills by 5%"],
+        ["2", "Max SP +6%, +recovery rate of recovery skills by 10%"],
+        ["3", "Max SP +9%, +recovery rate of recovery skills by 15%"],
+        ["4", "Max SP +12%, +recovery rate of recovery skills by 20%"],
+        ["5", "Max SP +15%, +recovery rate of recovery skills by 25%"],
+        ["6", "Max SP +18%, +recovery rate of recovery skills by 30%"],
+        ["7", "Max SP +21%, +recovery rate of recovery skills by 35%"],
+        ["8", "Max SP +24%, +recovery rate of recovery skills by 40%"],
+        ["9", "Max SP +27%, +recovery rate of recovery skills by 45%"],
+        ["10", "Max SP +30%, +recovery rate of recovery skills by 55%"],
+      ],
+    },
+  },
+  // Acute
+  {
+    name: "Acute",
+    table: {
+      columnHeaders: ["Level", "Enchant Description"],
+      rows: [
+        ["1", "CRIT +3%, +critical damage on targets by 20%"],
+        ["2", "CRIT +6%, +critical damage on targets by 35%"],
+        ["3", "CRIT +9%, +critical damage on targets by 50%"],
+        ["4", "CRIT +12%, +critical damage on targets by 65%"],
+        ["5", "CRIT +15%, +critical damage on targets by 80%"],
+        ["6", "CRIT +18%, +critical damage on targets by 95%"],
+        ["7", "CRIT +21%, +critical damage on targets by 110%"],
+        ["8", "CRIT +24%, +critical damage on targets by 125%"],
+        ["9", "CRIT +27%, +critical damage on targets by 140%"],
+        ["10", "CRIT +30%, +critical damage on targets by 170%"],
+      ],
+    },
+  },
+];
 </script>
 
 <template>
@@ -131,41 +266,98 @@ const table2021Enchantments = [
 
     <h3 class="">SEO: หมวก Lab 5, Headgear Enchantments, อ๊อฟหมวก</h3>
 
-    <!-- #region card -->
-    <div
-      v-for="enchantment in table2021Enchantments"
-      :key="enchantment.name"
-      class="p-3"
-    >
-      <div class="c-skill-card">
-        <h3 class="text-xl font-bold">{{ enchantment.name }}</h3>
+    <!-- #region card-20240621 -->
+    <div>
+      <h2 class="text-2xl font-bold">Update 20240621</h2>
+      <div
+        v-for="enchantment in table2021Enchantments"
+        :key="enchantment.name"
+        class="p-3"
+      >
+        <div class="c-skill-card">
+          <h3 class="text-xl font-bold">{{ enchantment.name }}</h3>
 
-        <!-- #region table -->
-        <table class="c-table">
-          <thead>
-            <tr>
-              <th class="c-table-th">
-                {{ enchantment.table.columnHeaders[0] }}
-              </th>
-              <th class="c-table-th">
-                {{ enchantment.table.columnHeaders[1] }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="row in enchantment.table.rows" :key="row[0]">
-              <td class="c-table-td tabular-nums">
-                {{ row[0] }}
-              </td>
-              <td class="c-table-td tabular-nums">
-                {{ row[1] }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <!-- #endregion table -->
+          <!-- #region table -->
+          <table class="c-table">
+            <thead>
+              <tr>
+                <th class="c-table-th">
+                  {{ enchantment.table.columnHeaders[0] }}
+                </th>
+                <th class="c-table-th">
+                  {{ enchantment.table.columnHeaders[1] }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="row in enchantment.table.rows" :key="row[0]">
+                <td class="c-table-td tabular-nums">
+                  {{ row[0] }}
+                </td>
+                <td class="c-table-td tabular-nums">
+                  {{ row[1] }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <!-- #endregion table -->
+        </div>
+      </div>
+    </div>
+    <!-- #endregion card-20240621 -->
+
+    <!-- #region card -->
+    <div>
+      <h2 class="text-2xl font-bold">Old</h2>
+      <div
+        v-for="enchantment in table2021Enchantments"
+        :key="enchantment.name"
+        class="p-3"
+      >
+        <div class="c-skill-card">
+          <h3 class="text-xl font-bold">{{ enchantment.name }}</h3>
+
+          <!-- #region table -->
+          <table class="c-table">
+            <thead>
+              <tr>
+                <th class="c-table-th">
+                  {{ enchantment.table.columnHeaders[0] }}
+                </th>
+                <th class="c-table-th">
+                  {{ enchantment.table.columnHeaders[1] }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="row in enchantment.table.rows" :key="row[0]">
+                <td class="c-table-td tabular-nums">
+                  {{ row[0] }}
+                </td>
+                <td class="c-table-td tabular-nums">
+                  {{ row[1] }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <!-- #endregion table -->
+        </div>
       </div>
     </div>
     <!-- #endregion card -->
+
+    <div>
+      <h2 class="text-2xl font-bold">References</h2>
+      <ul class="list-disc pl-6">
+        <li>
+          <a
+            href="https://ro.gnjoy.in.th/old-headgear-enchant-improve/"
+            target="_blank"
+          >
+            Old Headgear & Enchant Improve
+          </a>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
